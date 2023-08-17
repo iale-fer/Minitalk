04e18a2fff0391f297b6be67f3b212c42f1fcb94
Ivan Ale Fernandez <iale-fer@c3r3s4.42malaga.com>
2023-08-17 12:47:59 +0200

    Minitalk

--- client.c
+++ client.c
@@ -6,26 +6,49 @@
 /*   By: iale-fer <iale-fer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 16:46:36 by iale-fer          #+#    #+#             */
-/*   Updated: 2023/08/07 13:35:15 by iale-fer         ###   ########.fr       */
+/*   Updated: 2023/08/17 12:42:58 by iale-fer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "./libft/libft.h"
-// #include "minitalk.h"
+#include "./libft/libft.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+static void	ft_send_bit(int pid, char *str)
+{
+	int		bit;
+	char	character;
 
-void	obtenerYMostrarPID() {
-// Obtener el PID del proceso actual
-	pid_t pid = getpid();
-
-	printf("El PID del servidor es: %d\n", pid);
+	while (*str)
+	{
+		bit = 8;
+		character = *str;
+		while (bit--)
+		{
+			if (character >> bit & 1)
+				kill (pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+		str++;
+	}
 }
 
-int main() {
-// Llamada a la función para obtener y mostrar el PID
-	obtenerYMostrarPID();
-	return 0;
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*str;
+
+	if (argc != 3)
+	{
+		ft_printf("Wrong number of arguments. Need ./client <PID> <Message>\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		ft_send_bit(pid, str);
+	}
+	ft_send_bit(pid, "\n");
+	return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,14 +6,15 @@
 /*   By: iale-fer <iale-fer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 16:45:26 by iale-fer          #+#    #+#             */
-/*   Updated: 2023/08/04 10:24:24 by iale-fer         ###   ########.fr       */
+/*   Updated: 2023/08/17 12:43:49 by iale-fer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "./libft/libft.h"
-# include <signal.h>
+#include "../libft/libft.h"
+#include "../ft_printf/ft_printf.h"
+#include <signal.h>
 
 #endif
